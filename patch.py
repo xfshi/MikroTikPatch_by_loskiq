bab542ecaa745285af76ec792d3d4fe3fc466f6a
loskiq <loskiq@gmail.com>
2025-01-18 14:44:29 +0300

    Update patch.py

--- patch.py
+++ patch.py
@@ -96,22 +96,30 @@ def patch_block(dev: str, file: str, key_dict):
         print(']')
 
 
-def patch_initrd_xz(initrd_xz: bytes, key_dict: dict, ljust=True):
+def patch_initrd_xz(initrd_xz:bytes,key_dict:dict,ljust=True):
     initrd = lzma.decompress(initrd_xz)
-    new_initrd = initrd
-    for old_public_key, new_public_key in key_dict.items():
+    new_initrd = initrd  
+    for old_public_key,new_public_key in key_dict.items():
         if old_public_key in new_initrd:
             print(f'initrd public key patched {old_public_key[:16].hex().upper()}...')
-            new_initrd = new_initrd.replace(old_public_key, new_public_key)
-    new_initrd_xz = lzma.compress(new_initrd, check=lzma.CHECK_CRC32, filters=[
-                                  {"id": lzma.FILTER_LZMA2, "preset": 9, }])
+            new_initrd = new_initrd.replace(old_public_key,new_public_key)
+    preset = 6
+    new_initrd_xz = lzma.compress(new_initrd,check=lzma.CHECK_CRC32,filters=[{"id": lzma.FILTER_LZMA2, "preset": preset }] )
+    while len(new_initrd_xz) > len(initrd_xz) and preset < 9:
+        print(f'preset:{preset}')
+        print(f'new initrd xz size:{len(new_initrd_xz)}')
+        print(f'old initrd xz size:{len(initrd_xz)}')
+        preset += 1
+        new_initrd_xz = lzma.compress(new_initrd,check=lzma.CHECK_CRC32,filters=[{"id": lzma.FILTER_LZMA2, "preset": preset }] )
+    if len(new_initrd_xz) > len(initrd_xz):
+        new_initrd_xz = lzma.compress(new_initrd,check=lzma.CHECK_CRC32,filters=[{"id": lzma.FILTER_LZMA2, "preset": 9 | lzma.PRESET_EXTREME,'dict_size': 32*1024*1024,"lc": 4,"lp": 0, "pb": 0,}] )
     if ljust:
-        assert len(new_initrd_xz) <= len(
-            initrd_xz), 'new initrd xz size is too big'
+        print(f'preset:{preset}')
         print(f'new initrd xz size:{len(new_initrd_xz)}')
         print(f'old initrd xz size:{len(initrd_xz)}')
         print(f'ljust size:{len(initrd_xz)-len(new_initrd_xz)}')
-        new_initrd_xz = new_initrd_xz.ljust(len(initrd_xz), b'\0')
+        assert len(new_initrd_xz) <= len(initrd_xz),'new initrd xz size is too big'
+        new_initrd_xz = new_initrd_xz.ljust(len(initrd_xz),b'\0')
     return new_initrd_xz
 
 
